05a0afbf68ac3c38e9f5424d65dcb2a67c50983b
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-24 15:23:11 +0200

    ADD pseudo codce

---
+++ js/script.js
@@ -0,0 +1,24 @@
+console.log("Simon says");
+
+// GENERA 5 NUMERI CASUALI
+//  FOR 5 volte, genera numero casuale
+
+//  Mettere numero casuale in array
+
+//  Mostrare array
+
+// far partire un timer
+
+// nascondere numeri
+
+// mostrare form con input
+
+// Mandare input in un array
+
+// Confrontare l'array dei numeri casuali con quelli trovati dall'utente
+
+// Salvare in array i numeri in comune
+
+// OUTPUT
+// Quanti numeri hai trovato? (lunghezza nuovo array)
+// Quali? (posizione elementi in array)
